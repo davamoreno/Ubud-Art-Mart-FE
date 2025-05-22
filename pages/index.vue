8066ac47632099badee299a608d79d39c19e9574
davamoreno <davamoreno28@gmail.com>
2025-05-22 21:49:49 +0800

    Landing

--- pages/index.vue
+++ pages/index.vue
@@ -134,8 +134,9 @@
                         </div>
                     </div>
                 </div>
-          </div>
-     </div>
+            </div>
+        </div>
+    </div>
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
