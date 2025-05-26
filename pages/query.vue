9b92f35664add52992f2f1aaceaef805f5306f91
KevinAngga25 <anggarestakevin@gmail.com>
2025-05-26 13:18:10 +0800

    katalog search

---
+++ pages/query.vue
@@ -0,0 +1,167 @@
+<template>
+    <div>
+        <div class="container mx-auto px-[50px] pt-[113px] items-center h-full">
+            <div class="flex flex-row">
+                <div class="w-1/4">
+                    <h1>
+                        INI SIDE BAR
+                    </h1>
+                </div>
+                <div class="w-3/4">
+                    <div class="flex flex-wrap">
+
+                        <div class="w-1/4 mb-10 hover:scale-105 transition duration-300 ease-in-out">
+                            <div class="w-[250px] h-[313px] bg-white rounded-lg shadow-xl overflow-hidden">
+                                <img src="/assets/images/katalog.png" alt="Lukisan Bali"
+                                    class="w-full h-[220px] object-cover">
+                                <div class="p-4">
+                                    <div class="flex justify-between items-start">
+
+                                        <div>
+                                            <h2 class="text-lg font-semibold text-gray-800">Lukisan Bali</h2>
+                                            <p class="text-sm text-gray-600">Kios Pak Ketut</p>
+                                        </div>
+
+
+                                        <div class="flex items-center mt-4">
+                                            <img src="/assets/images/Star.svg" alt="" class="h-5 w-5">
+                                            <span class="ml-1 mt-1 text-sm text-gray-700 font-medium">3.9</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                        </div>
+
+                        <div class="w-1/4">
+                            <div class="w-[250px] h-[313px] bg-white rounded-lg shadow-lg overflow-hidden">
+                                <img src="/assets/images/katalog.png" alt="Lukisan Bali"
+                                    class="w-full h-[220px] object-cover">
+                                <div class="p-4">
+                                    <div class="flex justify-between items-start">
+
+                                        <div>
+                                            <h2 class="text-lg font-semibold text-gray-800">Lukisan Bali</h2>
+                                            <p class="text-sm text-gray-600">Kios Pak Ketut</p>
+                                        </div>
+
+
+                                        <div class="flex items-center mt-4">
+                                            <img src="/assets/images/Star.svg" alt="" class="h-5 w-5">
+                                            <span class="ml-1 mt-1 text-sm text-gray-700 font-medium">3.9</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                        </div>
+
+                        <div class="w-1/4">
+                            <div class="w-[250px] h-[313px] bg-white rounded-lg shadow-lg overflow-hidden">
+                                <img src="/assets/images/katalog.png" alt="Lukisan Bali"
+                                    class="w-full h-[220px] object-cover">
+                                <div class="p-4">
+                                    <div class="flex justify-between items-start">
+
+                                        <div>
+                                            <h2 class="text-lg font-semibold text-gray-800">Lukisan Bali</h2>
+                                            <p class="text-sm text-gray-600">Kios Pak Ketut</p>
+                                        </div>
+
+
+                                        <div class="flex items-center mt-4">
+                                            <img src="/assets/images/Star.svg" alt="" class="h-5 w-5">
+                                            <span class="ml-1 mt-1 text-sm text-gray-700 font-medium">3.9</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                        </div>
+
+                        <div class="w-1/4">
+                            <div class="w-[250px] h-[313px] bg-white rounded-lg shadow-lg overflow-hidden">
+                                <img src="/assets/images/katalog.png" alt="Lukisan Bali"
+                                    class="w-full h-[220px] object-cover">
+                                <div class="p-4">
+                                    <div class="flex justify-between items-start">
+
+                                        <div>
+                                            <h2 class="text-lg font-semibold text-gray-800">Lukisan Bali</h2>
+                                            <p class="text-sm text-gray-600">Kios Pak Ketut</p>
+                                        </div>
+
+
+                                        <div class="flex items-center mt-4">
+                                            <img src="/assets/images/Star.svg" alt="" class="h-5 w-5">
+                                            <span class="ml-1 mt-1 text-sm text-gray-700 font-medium">3.9</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                        </div>
+
+                        <div class="w-1/4">
+                            <div class="w-[250px] h-[313px] bg-white rounded-lg shadow-lg overflow-hidden">
+                                <img src="/assets/images/katalog.png" alt="Lukisan Bali"
+                                    class="w-full h-[220px] object-cover">
+                                <div class="p-4">
+                                    <div class="flex justify-between items-start">
+
+                                        <div>
+                                            <h2 class="text-lg font-semibold text-gray-800">Lukisan Bali</h2>
+                                            <p class="text-sm text-gray-600">Kios Pak Ketut</p>
+                                        </div>
+
+
+                                        <div class="flex items-center mt-4">
+                                            <img src="/assets/images/Star.svg" alt="" class="h-5 w-5">
+                                            <span class="ml-1 mt-1 text-sm text-gray-700 font-medium">3.9</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                        </div>
+
+                        <div class="w-1/4 mb-20">
+                            <div class="w-[250px] h-[313px] bg-white rounded-lg shadow-lg overflow-hidden">
+                                <img src="/assets/images/katalog.png" alt="Lukisan Bali"
+                                    class="w-full h-[220px] object-cover">
+                                <div class="p-4">
+                                    <div class="flex justify-between items-start">
+
+                                        <div>
+                                            <h2 class="text-lg font-semibold text-gray-800">Lukisan Bali</h2>
+                                            <p class="text-sm text-gray-600">Kios Pak Ketut</p>
+                                        </div>
+
+
+                                        <div class="flex items-center mt-4">
+                                            <img src="/assets/images/Star.svg" alt="" class="h-5 w-5">
+                                            <span class="ml-1 mt-1 text-sm text-gray-700 font-medium">3.9</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap"
+        rel="stylesheet">
+</template>
