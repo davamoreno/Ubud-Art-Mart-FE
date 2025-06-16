0d0d27585fe68d578039a04a414ccf93af7719f8
KevinAngga25 <anggarestakevin@gmail.com>
2025-06-16 18:42:48 +0800

    detail page dan perbaikan query

--- pages/query.vue
+++ pages/query.vue
@@ -3,13 +3,55 @@
         <div class="container mx-auto px-[50px] pt-[113px] items-center h-full">
             <div class="flex flex-row">
                 <div class="w-1/4">
-                    <h1>
-                        INI SIDE BAR
+                    <h1 class="font-bold text-lg">
+                        Filter
                     </h1>
+                    <div class="mt-[22px] w-[228px] h-[338px] p-[15px] border-2 rounded-lg shadow-md">
+                        <h1 class="font-bold mb-[15px] text-lg">Jenis Produk</h1>
+                        <form action="" class="flex flex-col mb-[40px]">
+                            <div class="flex items-center mb-[15px]">
+                                <input type="checkbox" name="patung" id="" class="me-[8px]">
+                                <label for="patung" class="font-bold">Patung</label>
+                            </div>
+
+                            <div class="flex items-center mb-[15px]">
+                                <input type="checkbox" name="lukisan" id="" class="me-[8px] font-bold">
+                                <label for="lukisan" class="font-bold">Lukisan</label>
+                            </div>
+
+                            <div class="flex items-center mb-[15px]">
+                                <input type="checkbox" name="aksesoris" id="" class="me-[8px]">
+                                <label for="aksesoris" class="font-bold">Aksesoris</label>
+                            </div>
+                        </form>
+                        <h1 class="font-bold mb-[15px] text-lg">Rating</h1>
+                        <form action="" class="flex flex-col">
+                            <div class="flex items-center mb-[15px]">
+                                <input type="checkbox" name="terendah" id="" class="me-[8px]">
+                                <label for="terendah" class="font-bold">Terendah</label>
+                            </div>
+
+                            <div class="flex items-center mb-[15px]">
+                                <input type="checkbox" name="tertinggi" id="" class="me-[8px] font-bold">
+                                <label for="tertinggi" class="font-bold">Tertinggi</label>
+                            </div>
+                        </form>
+                    </div>
                 </div>
                 <div class="w-3/4">
+                    <div
+                        class="relative py-[5px] px-[10px] mb-[52px] mx-auto w-full h-[50px] border border-slate-950 rounded-3xl">
+                        <div class="relative rounded-3xl w-[36px] h-[36px] bg-[#328E6E]">
+                            <img src="/assets/images/search.svg" alt="" class="absolute top-2 left-2">
+                        </div>
+                        <div class="absolute top-0 left-[60px] w-full h-full flex items-center">
+                            <form action="/query">
+                                <input type="text" name="" id="" placeholder="Cari di Katalog"
+                                    class="w-full h-full bg-transparent text-[#a8d4c5] placeholder:text-[#a8d4c5] focus:outline-none">
+                            </form>
+                        </div>
+                    </div>
                     <div class="flex flex-wrap">
-
                         <div class="w-1/4 mb-10 hover:scale-105 transition duration-300 ease-in-out">
                             <a href="/katalogView">
                                 <div class="w-[250px] h-[313px] bg-white rounded-lg shadow-xl overflow-hidden">
